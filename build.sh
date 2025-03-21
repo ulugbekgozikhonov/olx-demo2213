1fd35f8cc708560a58a0f0fa6ecf0fcebc6828b8
Ulug'bek G'ozixonov <ulugbekgozixonov@gmail.com>
2025-03-21 10:18:57 +0500

    changed build.sh

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py makemigrations
 python manage.py migrate
-if [[ $CREATE_SUPERUSER]];
+if [[ $CREATE_SUPERUSER]]
 then
     python manage.py createsuperuser --username admin --email 123@example.com
 fi
