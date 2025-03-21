17104045eaf60344ac768efa32acbebbec170d7e
Ulug'bek G'ozixonov <ulugbekgozixonov@gmail.com>
2025-03-21 10:16:35 +0500

    changed build.sh

--- build.sh
+++ build.sh
@@ -7,5 +7,5 @@ python manage.py makemigrations
 python manage.py migrate
 if [[ $CREATE_SUPERUSER]];
 then
-    python manage.py createsuperuser --no-input --email "$DJANGO_SUPERUSER_EMAIL"
+    python manage.py createsuperuser --username admin --email 123@example.com
 fi
